ea3bb60dff71cfae24c4fec00c4f0f48f374226b
Youjaeseon <wotjd0701@naver.com>
2026-08-25 21:25:58 +0900

    refactor: API명세 수정으로 인한 라우터 수정

--- routes/chat.py
+++ routes/chat.py
@@ -87,38 +87,3 @@ def _get_room_messages(room_id):
 #==========================================================================
 # 채팅목록
 #==========================================================================
-
-@chat_bp.route('/items/<item_id>')
-@login_required
-def item_chat(item_id):
-    try:
-        item_oid = ObjectId(item_id)
-    except Exception:
-        return "존재하지 않는 상품입니다.", 404
-
-    item = db.items.find_one({"_id": item_oid})
-
-    if not item:
-        return "존재하지 않는 상품입니다.", 404
-
-    # 내 상품일 경우
-    if item["seller_id"] == g.user[""]
-
-@chat_bp.route('/chats/<room_id>')
-def chat_room(room_id):
-    try:
-     room_oid = ObjectId(room_id)
-    except Exception:
-     return "존재하지 않는 채팅방입니다.", 404
-
-room = db.rooms.find_one({"_id": room_oid})
-if not room: 
-    return "존재하지 않는 채팅방입니다.", 404
-
-user_id = g.user["_id"]
-
-   
-
-@chat_bp.route("/api/rooms/<room_id>/messages", methods=["GET"])
-
-@chat_bp.route("/api/rooms/<room_id>/messages", methods=["POST"])
