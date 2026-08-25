e8fdf274bb9b2ffebfe57ac2bb0c739283a320eb
Youjaeseon <wotjd0701@naver.com>
2026-08-26 01:23:43 +0900

    feat: 파일 확장자 확인

--- routes/items.py
+++ routes/items.py
@@ -16,3 +16,22 @@ from werkzeug.utils import secure_filename
 
 from auth_util import login_required
 from db import db
+
+items_bp = Blueprint("items", __name__)
+
+ITEMS_PER_PAGE = 20
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif","webp"}
+
+#==========================================================================
+#공통함수
+#==========================================================================
+
+def to_active_id(value):
+    try:
+        return ObjectId(value)
+    except Exception:
+        return None
+
+    # 파일 확장자 확인
+def _allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
